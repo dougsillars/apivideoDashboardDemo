4275015cdc18ed1ad1949d288cba17b53509b86e
dougsillars <doug.sillars@gmail.com>
2020-06-11 19:44:05 +0200

    sql

--- src/sql.js
+++ src/sql.js
@@ -1,20 +1,20 @@
     //mysql for the productID -> api key in the dashboard demo
-     var mysql = require('mysql');
+     var {Client} = require('pg');
     
      var productIdSandbox = "prUMsehoEjCfbzxl1eeV00I";
     //now we have the productId for sand box and production
 	//lookup the api keys in the database
-	var con = mysql.createConnection({
-		host: "psql.api.video",
-		user: "libcast",
-		database: "subscription"
-	  });
-
-	  con.connect(function(err) {
-		if (err) throw err;
-		con.query("SELECT value from public.api_key where project_id ="+ productIdSandbox, function (err, result, fields) {
-		  if (err) throw err;
-		  console.log("sql query", result);
-		});
-	  });
-	
+	const client = new Client({
+        host: 'psql.api.video',
+        user: 'libcast',
+        Schema: public,
+        database: 'subscription',
+        password: 'secretpassword',
+        port: 3211,
+      })
+      client.connect()
+      client.query('SELECT value from public.api_key where project_id = '+ productIdSandbox, (err, res) => {
+        console.log(err, res)
+        client.end()
+      })
+     
